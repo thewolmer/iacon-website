0efb65fdfcdfcb7a9c03bddf8804972cd2511a9b
Wolmer <contact@wolmer.me>
2024-02-20 16:10:23 +0530

    build: config site using Nextjs
    Change site from vuejs to Nextjs &  Added Linting, Versioning, Components

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "types";
+
+export const siteConfig: SiteConfig = {
+  name: "ArmoBot",
+  description: "//",
+  url: "https://iaconlabs.com",
+  ogImage: "https://armobot.xyz/images/banner.png",
+  twitterHandle: "@IaconLabs",
+  links: {
+    twitter: "https://twitter.com/IaconLabs",
+  },
+};
