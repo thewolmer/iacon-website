e43bc55b66b7063490984846d7555b733e45a008
Wolmer <contact@wolmer.me>
2024-04-18 11:06:43 +0530

    fix: out dir name

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: 'dist',
+  reactStrictMode: false,
+  swcMinify: true,
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -11,6 +15,19 @@ const nextConfig = {
       },
     ],
   },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  // async redirects() {
+  //   return [{ source: '/collection/', destination: '/', permanent: false }];
+  // },
+  // webpack: (config) => {
+  //   config.resolve.fallback = { fs: false, net: false, tls: false };
+  //   config.externals.push('pino-pretty', 'lokijs', 'encoding');
+  //   return config;
+  // },
 };
 
 export default nextConfig;
