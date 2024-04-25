73be4d6205ba777498e2fe11d8bb36efcb59a043
Wolmer <contact@wolmer.me>
2024-04-25 12:54:14 +0530

    fix: oG Image and Footer fix

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -13,9 +13,9 @@ export const Footer = () => (
         </div>
       </Link>
     </div>
-    <div className="flex flex-col items-center justify-center p-2 font-mono">
-      <p> &copy; {new Date().getFullYear()} Iacon Labs</p>
-      <p className="opacity-55">
+    <div className="mb-2 flex flex-col items-center justify-center p-2 ">
+      <p className="font-mono text-lg"> &copy; {new Date().getFullYear()} Iacon Labs</p>
+      <p className="  text-sm font-extralight opacity-70">
         Website by -{' '}
         <Link className="underline underline-offset-4" href="https://wolmer.me">
           wolmer.me
